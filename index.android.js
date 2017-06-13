d46fde8b1157c3e2b8a184f7f636e98d0480500b
Subhash Pandey <Pandeysu@xuber.com>
2017-06-13 12:41:04 +0530

    RR

---
+++ index.android.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+export default class RR extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome 
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit me
+        </Text>
+        <Text style={styles.instructions}>
+          Double tap R on your keyboard to reload
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('RR', () => RR);
